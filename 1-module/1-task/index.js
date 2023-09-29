3630acbfbeeb5710c65290d035ece5fded8baa9e
Radik Zainullin <radik.zainullin@gmail.com>
2023-09-29 07:19:20 +0300

    Задача вычисления факториала решена.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  // будем считать что входной параметр есть положительное число или ноль
+  if (n === 0) return 1;
+  if (n === 1) return 1;
+  let accumulator = 1;
+  for (let i = 1; i <= n; i++) {
+    accumulator = accumulator*i;
+  }
+  return accumulator;
 }
