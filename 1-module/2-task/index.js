ee6bd220b2bef038c3a1fcacd34c1e0077545222
Radik Zainullin <radik.zainullin@gmail.com>
2023-09-30 21:37:20 +0300

    функция проверка имени сделана

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,8 +9,21 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
+/**
+ * Необходимо, чтобы на сайте можно было поприветствовать только пользователей, 
+ * которые удовлетворяют следующему условию – имя не пустое, без пробелов, минимум 4 символа. 
+ */
 function isValid(name) {
-  // ваш код...
+  // проверка аргумента, что он не пустой и не null
+  if (arguments.length === 0 || arguments[0] === null) return false;
+  // проверка есть ли пробелы
+  const nameArr = name.split("");
+  for (let i = 0; i < nameArr.length; i++) {
+    if (nameArr[i] === ' ') return false;
+  }
+  // name должен быть минимум четыре символа
+  if (nameArr.length >= 4) return true;
+  else return false;
 }
 
 function sayHello() {
